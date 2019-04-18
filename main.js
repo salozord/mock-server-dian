d95f9429e061f833598c0f092bf186edce4be21c
Santiago Salazar <s.salazara@uniandes.edu.co>
2019-04-17 22:47:31 -0500

    Cambios para que sirva. Falta implementación del POST.

--- main.js
+++ main.js
@@ -1,20 +1,35 @@
 const express = require('express');
 const fs = require('fs');
-var https = require('https');
+const x509 = require('x509.js');
+const crypto = require('crypto');
+const hash = crypto.createHash('sha256');
+const https = require('https');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-//Punto de acceso raíz
+var certificado = x509.parseCert(fs.readFileSync('files/mock.cert'));
+// var llavePrivada = x509.parseKey(fs.readFileSync('files/mock.key'));
+
+//Punto de acceso raíz (no hace nada realmente)
 app.get('/', function(req, res) {
-    console.log('[GET] (ruta: "")');
-    res.send('Servidor Mock DIAN sirviendo!');
+    console.log('[GET] (ruta: "/")');
+    res.status(200).send('Servidor Mock DIAN sirviendo!');
 });
 
-//Punto de acceso para dar el certificado
+//Punto de acceso para dar el certificado si es necesario
 app.get('/certificate', function (req, res) {
-    console.log('GET - inició petición por el certificado');
-    
+    console.log('[GET] (ruta: "/certificate") - inició petición por el certificado');
+
+    let cert = fs.readFileSync('files/mock.cert').toString('utf-8');
+    res.status(200).send(cert);
+
+    console.log('[GET] (ruta: "/certificate") - ¡Certificado enviado éxitosamente! - código: 200');
+});
+
+// Punto de acceso para recibir las facturas
+app.post('/api', function (req, res) {
+    //COSAS PARA FACTURAS Y VERIFICAR !!!
 });
 
 https.createServer({
